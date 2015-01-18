221d21e4242aac897b1e702e2f3a9ccc124a26e9
Dora <dora2411@gmail.com>
2015-01-18 09:44:12 +0100

    Made it bit faster

--- lzwkoder.c
+++ lzwkoder.c
@@ -17,7 +17,7 @@ struct node *root;
 struct trie_node {
 	unsigned short int index;
 	char value;
-	struct trie_node *link[DICT_SIZE];
+	struct trie_node *link[ALPH_SIZE];
 };
 
 struct trie_node *trie_root = NULL;
@@ -25,7 +25,7 @@ struct trie_node *trie_root = NULL;
 
 struct trie_node *create_node() {
     struct trie_node *q = (struct trie_node*) malloc( sizeof(struct trie_node) );
-    for(int x=0 ; x < DICT_SIZE ; x++)
+    for(int x=0 ; x < ALPH_SIZE ; x++)
         q->link[x] = NULL;
     q->index = q->value = -1;
     return q;
@@ -65,7 +65,8 @@ void get_input(const char * argv[])
    			new->next = NULL;
    			walker->character = character;
    			walker->next = new;
-   			walker = walker->next;   		}
+   			walker = walker->next;
+   		}
    	}
     walker = NULL;
    	fclose(f);
@@ -79,13 +80,11 @@ void add_word(struct trie_node *monkey, struct node *walker, int i)
     monkey->link[i]->index = counter;
     counter++;
     fwrite(&monkey->index, sizeof(unsigned short), 1, writer);
-    //printf("%hu ", monkey->index);
 }
 
 
 void code()
 {
-    //get_input();
 	struct node *walker;
 	struct trie_node *monkey;
     
@@ -94,7 +93,7 @@ void code()
     monkey = trie_root;
 	do {
 		int i = 0;
-		while ((monkey->link[i] != NULL) && (i < DICT_SIZE)){
+		while ((monkey->link[i] != NULL) && (i < ALPH_SIZE)){
 			if (monkey->link[i]->value == walker->character) {
 				if (walker->next != NULL && walker->next->character != '\0') {
 					walker = walker->next;
@@ -103,14 +102,13 @@ void code()
 				}
 				else {
 					fwrite(&monkey->link[i]->index, sizeof(unsigned short), 1, writer);
-					//printf("%hu ", monkey->link[i]->index);
 					return;
 				}
 			}
 			else
                 i++;
 		}
-		if (counter < 65535) {
+		if (counter < DICT_SIZE) {
         	add_word(monkey, walker, i);
     	}
     	else {
@@ -118,10 +116,9 @@ void code()
     	}
 		monkey = trie_root;
 	} while (walker->next != NULL && walker->next->character != '\0');
-	for (int i = 0 ; i < DICT_SIZE; i++){
+	for (int i = 0 ; i < ALPH_SIZE; i++){
 		if (monkey->link[i]->value == walker->character) {
 			fwrite(&monkey->link[i]->index, sizeof(unsigned short), 1, writer);
-			//printf("%hu ", monkey->link[i]->index);
 			return;
 		}
 	}
